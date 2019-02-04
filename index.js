5612a293d85b3bc4cdddcfbc76a177f378ae3deb
Unknown <bogin7@gmail.com>
2019-02-04 08:47:34 +0200

    safer download

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const fsex = require("fs-extra");
 const path = require("path");
 const uuidv4 = require('uuid/v4');
 const express = require('express')
@@ -7,13 +8,27 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 let download = async (packages, path) => {
+    let results = {failes: []};
     for(let package of packages) {
-        let s = `node "${__dirname}/node_modules/node-tgz-downloader/bin/download-tgz" package ${package.name} --directory "${path}"`;
-        await exec(s, {
-            pwd: path
-        });
+        try {
+            let s = `node "${__dirname}/node_modules/node-tgz-downloader/bin/download-tgz" package ${package.name} --directory "${path}"`;
+            let {stdout, stderr} = await exec(s, {
+                pwd: path
+            });
+            console.log(stdout);
+            if(stderr) {
+                console.error(stderr);
+                results.failes.push(package);
+            }
+            else if(!fs.existsSync(`${path}/${package.name}`)) {
+                results.failes.push(package);
+            }
+        } catch(e) {
+            results.failes.push(package)
+        }
         
     }
+    return results;
 };
 
 async function main() {
@@ -45,24 +60,45 @@ function zipDirectory(source, out) {
 const app = express()
 const port = process.env.PORT || 8080;
 
+function formatInput(input) {
+    return input.split(",").map((pack) => {
+        let packsplit = pack.split("@");
+        return ({
+            name: packsplit[0], 
+            version: packsplit.length > 1 ? packsplit[1] : undefined
+        })
+    })
+}
+
 app.get('/', async (req, res) => {
     if(req.query.package) {
-        let folder = uuidv4();
-        let path =  __dirname+"/tarballs/"+folder;
-        let finalPaths = __dirname+"/finals";
-        let finalPath = finalPaths+"/"+folder+".zip";
-        fs.mkdirSync(path, { recursive: true });
-        fs.mkdirSync(finalPaths, { recursive: true });
-        console.log("download!");
-        await download(
-            req.query.package.split(",").map((pack) => ({
-                name: pack, 
-                version: undefined
-            })), path);
-        console.log("zip!");
-        await zipDirectory(path, finalPath);
-        console.log("send results");
-        res.download(finalPath, folder+".zip");
+        try {
+            let folder = uuidv4();
+            let path =  __dirname+"/tarballs/"+folder;
+            let finalPaths = __dirname+"/finals";
+            let finalPath = finalPaths+"/"+folder+".zip";
+            fs.mkdirSync(path, { recursive: true });
+            fs.mkdirSync(finalPaths, { recursive: true });
+            console.log("download!");
+            let results = await download(
+                formatInput(req.query.package), path);
+            if(results.failes.length > 0) {
+                console.log(JSON.stringify(results.failes));
+                res.send("Couldn't download packages: "+results.failes.map(package => `${package.name}${package.version ? "@"+package.version :""}`).join(","));
+            } else {
+                console.log("zip!");
+                await zipDirectory(path, finalPath);
+                console.log("send results");
+                res.download(finalPath, folder+".zip", function(err) {
+                    fsex.removeSync(path);
+                    fsex.removeSync(finalPath);
+                });
+               
+            }
+        } catch(e) {
+            res.send("Couldn't download files");
+            console.error(e);
+        }
     } else {
         res.sendFile(path.join(__dirname+'/package.html'));
     }
